5c80dcf95a5211aa24e7175e9f762e4a2277f564
SmituS <mixercool1@gmail.com>
2021-09-10 23:31:09 +0300

    {feature} [urls views] add search in library

--- library/views.py
+++ library/views.py
@@ -29,13 +29,21 @@ def index(request):
     return HttpResponse("welcome to base root of library api")
 
 
-def library(request, author='', name=''):
+def library(request):
     books = Book.objects.all()
 
-    paginator = Paginator(books, 4)  # Show 25 contacts per page.
-
     page_number = request.GET.get('page')
+    author_filter = request.GET.get('author')
+    book_name_filter = request.GET.get('title')
+
+    if book_name_filter:
+        books = books.filter(book_name__icontains=book_name_filter)
+
+    if author_filter:
+        authors = Author.objects.filter(author_name__icontains=author_filter)
+        books = books.filter(book_authors__in=authors)
+
+    paginator = Paginator(books, 4)
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'list.html', {'page_obj': page_obj})
-    #return Response('list.html', {'page_obj': page_obj})
